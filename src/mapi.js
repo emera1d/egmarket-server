17a6a8d9d77330294ea63dda6c94ad4855a02435
emera1d <nazivan@gmail.com>
2022-09-21 01:12:08 +0300

    tkn

--- src/mapi.js
+++ src/mapi.js
@@ -116,19 +116,19 @@ class CMapi {
 				const sid = session.make();
 				await database.updateProfile({ profileId }, { sid, isAuth: true, otp: '' });
 
-				const maxAge = 30 *24 *60 *60 *1000;
-				// const maxAge = 90000
-				res.cookie(appConfig.SID_COOKIE_NAME, sid, {
-					// httpOnly: false,
-					httpOnly: true,
-					maxAge,
-					sameSite: 'none',
-					secure: true,
-				});
+				// const maxAge = 30 *24 *60 *60 *1000;
+				// // const maxAge = 90000
+				// res.cookie(appConfig.SID_COOKIE_NAME, sid, {
+				// 	// httpOnly: false,
+				// 	httpOnly: true,
+				// 	maxAge,
+				// 	sameSite: 'none',
+				// 	secure: true,
+				// });
 				return { 
 					success: true,
 					profile: this._clientProfile(profile),
-					// token: sid,
+					token: sid,
 				};
 			} else {
 				return { error: 'validation', isValidProfile, isValidProfilePassword };
@@ -307,9 +307,9 @@ class CMapi {
 	}
 
 	async _getSessionProfile(req) {
-		const sid = req.cookies[appConfig.SID_COOKIE_NAME];
-		// const { token } = req.body;
-		// const sid = token;
+		// const sid = req.cookies[appConfig.SID_COOKIE_NAME];
+		const { token } = req.body;
+		const sid = token;
 
 		if (sid) {
 			const sessionData = session.get(sid);
